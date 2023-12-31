6bf7ac276e01ff56d11c799dbc011e00d3f9f742
erdi ergene <erdieergene@gmail.com>
2023-12-31 17:43:18 +0300

    fix

--- AzureOrnek/ViewController.swift
+++ AzureOrnek/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Log erdi")
+        print("Log erdi2")
         // Do any additional setup after loading the view.
     }
 
